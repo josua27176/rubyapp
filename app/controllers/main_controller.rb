3efb3b3e9c95808a838b7c31d979cee113046d50
Josua27176 <joshua27176@gmail.com>
2023-05-02 15:19:35 -0400

    Added Login and Logout with Flash Popups

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,5 @@
 class MainController < ApplicationController
 
   def index
-    flash.now[:notice]
-    flash.now[:alert]
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
   end
 end
